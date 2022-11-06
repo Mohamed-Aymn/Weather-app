ead2cc26432b80ccd8f9e2938b9f8da3449daaf2
Mohamed Aymn <mohamedaymn218@gmail.com>
2022-11-06 13:37:38 +0200

    enhanced readability

--- index.js
+++ index.js
@@ -3,16 +3,130 @@ const api = {
     base: "https://api.openweathermap.org/data/2.5/",
 };
 
+// conditional cards display (landing card, info card, error card)
 let mainContainer = document.querySelector(".contentContainer");
 let landingContainer = document.querySelector(".landingContainer");
 let errorContainer = document.querySelector(".errorContainer");
 let errorMessage = document.querySelector(".errorMessage");
-
-let errorContainerHandler = (errorInput) => {
+let errorContainerTriggerer = (errorInput) => {
     errorContainer.classList.remove("hidden");
     mainContainer.classList.add("hidden");
     landingContainer.classList.add("hidden");
-    errorMessage.innerText = `There is no city or country called "${errorInput}"`;
+    document.body.classList.remove("darkTheme");
+    if (errorInput !== "") {
+        errorMessage.innerHTML = `There is <b>no</b> city or country called "<b>${errorInput}</b>".`;
+    } else {
+        errorMessage.innerHTML = `Please type a name of city or country before pressing <b>enter</b> key.`;
+    }
+};
+
+// city name setter
+let city = document.querySelector(".city");
+let setCityName = (data) => {
+    city.innerText = `${data.name}, ${data.sys.country}`;
+};
+
+// temp setter
+let temp = document.querySelector(".temp");
+let setTemp = (data) => {
+    temp.innerHTML = `${Math.floor(data.main.temp)}°C`;
+};
+
+// hilow setter
+let hiLow = document.querySelector(".hiLow");
+let hiLowContainer = document.querySelector(".hiLowContainer");
+let setHighLow = (data) => {
+    if (
+        Math.floor(data.main.temp_min) === Math.floor(data.main.temp_max) ||
+        data.main.temp_min === 0 ||
+        data.main.temp_max === 0
+    ) {
+        hiLowContainer.classList.add("hidden");
+    } else {
+        hiLowContainer.classList.remove("hidden");
+        hiLow.innerHTML = `${Math.floor(data.main.temp_min)} / ${Math.floor(
+            data.main.temp_max
+        )}`;
+    }
+};
+
+// icon setter
+let setIcon = (icon) => {
+    document.querySelector(
+        ".icon"
+    ).src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+};
+
+//  wind setter
+let wind = document.querySelector(".wind");
+let windContainer = document.querySelector(".windContainer");
+let setWind = (data) => {
+    if (data.wind.speed === 0) {
+        windContainer.classList.add("hidden");
+    } else {
+        windContainer.classList.remove("hidden");
+        wind.innerHTML = `${data.wind.speed} km/h`;
+    }
+};
+
+// humidity setter
+let humidity = document.querySelector(".humidity");
+let setHumidity = (data) => {
+    humidity.innerHTML = `${data.main.humidity}%`;
+};
+
+// time setter
+let time = document.querySelector(".time");
+let setTime = (data) => {
+    setInterval(
+        (function exactTime() {
+            time.innerHTML = `${moment
+                .utc()
+                .add(data.timezone, "seconds")
+                .format("h:m:ss A")}`;
+            return exactTime;
+        })(),
+        1000
+    );
+};
+
+// sunrise and sunset setter
+let sunrise = document.querySelector(".sunrise");
+let sunset = document.querySelector(".sunset");
+let setSunriseSunset = (data) => {
+    sunrise.innerHTML = `Sunrise: ${moment
+        .utc(data.sys.sunrise, "X")
+        .add(data.timezone, "seconds")
+        .format("h:mm A")}`;
+    sunset.innerHTML = `Sunset: ${moment
+        .utc(data.sys.sunset, "X")
+        .add(data.timezone, "seconds")
+        .format("h:mm A")}`;
+};
+
+// date setter
+let date = document.querySelector(".date");
+let setDate = (data) => {
+    date.innerHTML = `${moment.utc().add(data.timezone, "date").format("LL")}`;
+};
+
+// background setter
+let setBackgroundColor = (data) => {
+    let nightTime = parseInt(
+        moment
+            .utc(data.sys.sunset, "X")
+            .add(data.timezone, "seconds")
+            .format("h")
+    );
+    let currentHour = parseInt(
+        moment.utc().add(data.timezone, "seconds").format("h")
+    );
+
+    if (currentHour >= nightTime) {
+        document.body.classList.add("darkTheme");
+    } else {
+        document.body.classList.remove("darkTheme");
+    }
 };
 
 addEventListener("load", () => {
@@ -26,13 +140,14 @@ addEventListener("load", () => {
                 .then((data) => {
                     return data.json();
                 })
-                .then(displayResults);
-            landingContainer.classList.add("hidden");
-            mainContainer.classList.remove("hidden");
+                .then(displayResults)
+                .then(() => {
+                    landingContainer.classList.add("hidden");
+                    mainContainer.classList.remove("hidden");
+                });
         },
         // denied position
         () => {
-            // console.log(flase);
             mainContainer.classList.add("hidden");
         }
     );
@@ -57,92 +172,25 @@ function getResults(query) {
         .then((data) => {
             displayResults(data);
             errorContainer.classList.add("hidden");
-            if (mainContainer.classList.contains("hidden")) {
-                mainContainer.classList.remove("hidden");
-            }
+            landingContainer.classList.add("hidden");
+            mainContainer.classList.remove("hidden");
         })
         .catch(() => {
-            errorContainerHandler(query);
+            errorContainerTriggerer(query);
         });
 }
 
 function displayResults(data) {
-    // city name
-    let city = document.querySelector(".city");
-    city.innerText = `${data.name}, ${data.sys.country}`;
-
-    // temp
-    let temp = document.querySelector(".temp");
-    temp.innerHTML = `${Math.floor(data.main.temp)}°C`;
-
-    // hiLow
-    let hiLow = document.querySelector(".hiLow");
-    hiLow.innerHTML = `${Math.floor(data.main.temp_min)} / ${Math.floor(
-        data.main.temp_max
-    )}`;
-
-    // icon
-    document.querySelector(
-        ".icon"
-    ).src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-
-    // wind
-    let wind = document.querySelector(".wind");
-    wind.innerHTML = `${data.wind.speed} km/h`;
-
-    // humidity
-    let humidity = document.querySelector(".humidity");
-    humidity.innerHTML = `${data.main.humidity}%`;
-
-    // time
-    let time = document.querySelector(".time");
-    setInterval(
-        (function exactTime() {
-            time.innerHTML = `${moment
-                .utc()
-                .add(data.timezone, "seconds")
-                .format("h:m:ss A")}`;
-            return exactTime;
-        })(),
-        1000
-    );
-
-    // date
-    let date = document.querySelector(".date");
-    date.innerHTML = `${moment.utc().add(data.timezone, "date").format("LL")}`;
-
-    // sunrise
-    let sunrise = document.querySelector(".sunrise");
-    let sunriseExactTime = moment
-        .utc(data.sys.sunrise, "X")
-        .add(data.timezone, "seconds")
-        .format("h:mm A");
-    sunrise.innerHTML = `Sunrise: ${sunriseExactTime}`;
-
-    // sunset
-    let sunset = document.querySelector(".sunset");
-    let sunsetExactTime = moment
-        .utc(data.sys.sunset, "X")
-        .add(data.timezone, "seconds")
-        .format("h:mm A");
-    sunset.innerHTML = `Sunset: ${sunsetExactTime}`;
-
-    // background color
-    let nightTime = parseInt(
-        moment
-            .utc(data.sys.sunset, "X")
-            .add(data.timezone, "seconds")
-            .format("h")
-    );
-    let currentHour = parseInt(
-        moment.utc().add(data.timezone, "seconds").format("h")
-    );
-
-    if (currentHour >= nightTime) {
-        document.body.classList.add("darkTheme");
-    } else {
-        document.body.classList.remove("darkTheme");
-    }
+    setCityName(data);
+    setTemp(data);
+    setHighLow(data);
+    setIcon(data.weather[0].icon);
+    setWind(data);
+    setHumidity(data);
+    setTime(data);
+    setDate(data);
+    setSunriseSunset(data);
+    setBackgroundColor(data);
 }
 
 // function calcTime(offset) {
